6cb2744a733da6166fc465dac5ea875db55258ae
Yogesh Palve <dnyyogesh@gmail.com>
2025-07-26 01:30:20 +0530

    auth api for admin created

--- backend/src/Routes/auth/adminAuth.route.ts
+++ backend/src/Routes/auth/adminAuth.route.ts
@@ -6,6 +6,11 @@ import {
   userLoginValidator,
 } from "../../validations/authValidator";
 import authMiddleware from "../../middlewares/authMiddleware";
+import {
+  resetPassword,
+  sendOtp,
+  updateProfile,
+} from "../../Controllers/auth/user.controller";
 
 const router = Router();
 
@@ -17,5 +22,12 @@ router.post(
   addUser
 );
 router.post("/login", userLoginValidator, amdinLogin);
-
+router.post("/send-otp", sendOtp);
+router.post("/reset-password", resetPassword);
+router.post(
+  "/udpate-profile",
+  authMiddleware.authenticateToken,
+  authMiddleware.verifyIntern,
+  updateProfile
+);
 export default router;
