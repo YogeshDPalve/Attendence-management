7ff46523cfeab783a692bbeb68c73bd6c797229a
harshadshela <harshadshelar205@gmail.com>
2025-08-14 14:07:28 +0530

    harshad

--- frontend/src/components/leave-form.jsx
+++ frontend/src/components/leave-form.jsx
@@ -75,7 +75,7 @@ export function LeaveForm({ className, ...props }) {
                       <SelectItem value="pineapple">Other</SelectItem>
                     </SelectGroup>
                   </SelectContent>
-                </Select>
+                </Select>         
               </div>
               {/* <div className="flex flex-col gap-2">
                 <div className="flex items-center">
